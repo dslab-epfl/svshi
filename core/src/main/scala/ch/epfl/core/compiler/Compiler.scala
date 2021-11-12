c0441bb514c9afd74a4bed9184eccc80eb020868
samuelchassot <samuel.chassot@epfl.ch>
2021-11-12 09:43:46 +0100

    todo comment

--- core/src/main/scala/ch/epfl/core/compiler/Compiler.scala
+++ core/src/main/scala/ch/epfl/core/compiler/Compiler.scala
@@ -4,6 +4,11 @@ import ch.epfl.core.models.ApplicationLibrary
 
 object Compiler {
   def compile(library: ApplicationLibrary) : ApplicationLibrary = {
+    //TODO
+    // here we need to read assignment of physical communicationObjects to XKNX stuff
+    // assign Group addresses to communicationObject
+    // generate files for the python apps with the group addresses
+    // generate files for the KNX programmming module
     library
   }
 
