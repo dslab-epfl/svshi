af4f64f723e340d5bab17371d7e96e3d3e5ba075
samuelchassot <samuel.chassot@epfl.ch>
2021-11-12 12:56:54 +0100

    add humidity and temperature sensors

--- core/src/main/scala/ch/epfl/core/models/prototypical/SupportedDevice.scala
+++ core/src/main/scala/ch/epfl/core/models/prototypical/SupportedDevice.scala
@@ -6,9 +6,13 @@ sealed trait SupportedDevice {
 object SupportedDevice {
   final val binarySensorString = "binary"
   final val switchString = "switch"
+  final val temperatureSensor = "temperature"
+  final val humiditySensor = "humidity"
   def fromString(s: String) : SupportedDevice = s.toLowerCase match {
     case SupportedDevice.binarySensorString => BinarySensor
     case SupportedDevice.switchString => Switch
+    case SupportedDevice.temperatureSensor => TemperatureSensor
+    case SupportedDevice.humiditySensor => HumiditySensor
     case _ => throw new UnsupportedDeviceException(s"The device $s is not supported by Pistis!")
   }
 }
@@ -18,6 +22,12 @@ case object BinarySensor extends SupportedDevice {
 }
 case object Switch extends SupportedDevice {
   override def toString: String = SupportedDevice.switchString
+}
+case object TemperatureSensor extends SupportedDevice {
+  override def toString: String = SupportedDevice.temperatureSensor
+}
+case object HumiditySensor extends SupportedDevice {
+  override def toString: String = SupportedDevice.humiditySensor
 }
 
 class UnsupportedDeviceException(msg: String) extends Exception(msg)
