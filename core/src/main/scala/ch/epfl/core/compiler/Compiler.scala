7ed1f5eb706116b7efe6e70d8ea6fc8dd41934ea
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-11-15 18:08:35 +0100

    Add call to Programmer in Compiler

--- core/src/main/scala/ch/epfl/core/compiler/Compiler.scala
+++ core/src/main/scala/ch/epfl/core/compiler/Compiler.scala
@@ -7,6 +7,7 @@ import ch.epfl.core.compiler.parsers.json.physical.PhysicalStructureJsonParser
 import ch.epfl.core.models.application.ApplicationLibrary
 import ch.epfl.core.models.physical._
 import ch.epfl.core.utils.Constants
+import ch.epfl.core.compiler.programming.Programmer
 
 object Compiler {
   def compile(library: ApplicationLibrary, physicalStructure: PhysicalStructure): ApplicationLibrary = {
@@ -15,42 +16,15 @@ object Compiler {
     // assign Group addresses to communicationObject
     // generate files for the python apps with the group addresses
     // generate files for the KNX programmming module
-    // val struct = PhysicalStructure(
-    //   PhysicalDevice(
-    //     "device1",
-    //     ("1", "2", "3"),
-    //     PhysicalDeviceNode(
-    //       "node1",
-    //       PhysicalDeviceCommObject("commObj1", KNXDatatype.fromString("DPT-1").get, IOType.fromString("in").get, 1) :: PhysicalDeviceCommObject(
-    //         "commObj10",
-    //         KNXDatatype.fromString("DPT-12").get,
-    //         IOType.fromString("in").get, 2
-    //       ) :: Nil
-    //     ) :: Nil
-    //   ) :: PhysicalDevice(
-    //     "device2",
-    //     ("1", "2", "4"),
-    //     PhysicalDeviceNode("node2", PhysicalDeviceCommObject("commObj2", KNXDatatype.fromString("DPT-2").get, IOType.fromString("in/out").get, 3) :: Nil) :: Nil
-    //   ) :: Nil
-    // )
-    // val bindings = AppLibraryBindings(
-    //   AppPrototypeBindings(
-    //     "test",
-    //     DeviceInstanceBinding("device1", TemperatureSensorBinding("temp", 1)) :: DeviceInstanceBinding("device2", SwitchBinding("switch", 2, 3)) :: Nil
-    //   ) :: Nil
-    // )
-    // val mapping = Map(1 -> GroupAddress(1, 1, 1), 2 -> GroupAddress(1, 2, 1), 3 -> GroupAddress(1, 2, 2))
-    // Programmer.outputProgrammingFile(GroupAddressAssignment(struct, bindings, mapping))
-
 
     val appLibraryBindings = BindingsJsonParser.parse(Constants.APP_LIBRARY_FOLDER_PATH + Constants.APP_PROTO_BINDINGS_JSON_FILE_NAME)
     val gaAssignment = GroupAddressAssigner.assignGroupAddressesToPhysical(physicalStructure, appLibraryBindings)
-    for(app <- library.apps){
+    for (app <- library.apps) {
       val pythonAddr = PythonAddressJsonParser.assignmentToPythonAddressJson(app, gaAssignment)
       PythonAddressJsonParser.writeToFile(app.appFolderPath + Constants.APP_PYTHON_ADDR_BINDINGS_FILE_NAME, pythonAddr)
     }
 
-    // TODO Call the KNX Programing ? or return something
+    Programmer.outputProgrammingFile(gaAssignment)
 
     library
   }
