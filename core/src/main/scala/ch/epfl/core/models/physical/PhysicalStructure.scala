1cba098eb5f2cebc4f5d74268fb6cf8375d56e85
samuelchassot <samuel.chassot@epfl.ch>
2021-11-15 16:13:38 +0100

    move hash computation in an factory method for physicalCommObject

--- core/src/main/scala/ch/epfl/core/models/physical/PhysicalStructure.scala
+++ core/src/main/scala/ch/epfl/core/models/physical/PhysicalStructure.scala
@@ -1,6 +1,12 @@
 package ch.epfl.core.models.physical
 
+import scala.util.hashing.MurmurHash3
+
 case class PhysicalStructure(deviceInstances: List[PhysicalDevice])
 case class PhysicalDevice(name: String, address: (String, String, String), nodes: List[PhysicalDeviceNode])
 case class PhysicalDeviceNode(name: String, comObjects: List[PhysicalDeviceCommObject])
 case class PhysicalDeviceCommObject(name: String, datatype: KNXDatatype, ioType: IOType, id: Int)
+object PhysicalDeviceCommObject{
+  def from(name: String, datatype: KNXDatatype, ioType: IOType): PhysicalDeviceCommObject = PhysicalDeviceCommObject(name, datatype, ioType, MurmurHash3.listHash(List(name, datatype, ioType), MurmurHash3.seqSeed))
+}
+
