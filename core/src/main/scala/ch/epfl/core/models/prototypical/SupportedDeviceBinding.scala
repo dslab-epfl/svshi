a5072543948d9b1c32864faf83d828e11a9e5792
samuelchassot <samuel.chassot@epfl.ch>
2021-11-15 17:06:44 +0100

    group address assigner + test

--- core/src/main/scala/ch/epfl/core/models/prototypical/SupportedDeviceBinding.scala
+++ core/src/main/scala/ch/epfl/core/models/prototypical/SupportedDeviceBinding.scala
@@ -6,27 +6,37 @@ import upickle.default.{ReadWriter, macroRW}
 // WARNING!!!!!!
 // argument names must be different for all types for uPickle to be able to discriminate them
 
-sealed trait SupportedDeviceBinding
+sealed trait SupportedDeviceBinding {
+  def getBoundIds: List[Int]
+}
 object SupportedDeviceBinding {
   implicit val rw: ReadWriter[SupportedDeviceBinding] = ReadWriter.merge(BinarySensorBinding.rw, SwitchBinding.rw, TemperatureSensorBinding.rw, HumiditySensorBinding.rw)
 }
 
-case class BinarySensorBinding(typeString: String, physDeviceId: Int) extends SupportedDeviceBinding
+case class BinarySensorBinding(typeString: String, physDeviceId: Int) extends SupportedDeviceBinding {
+  override def getBoundIds: List[Int] = List(physDeviceId)
+}
 object BinarySensorBinding {
   implicit val rw: ReadWriter[BinarySensorBinding] =
     macroRW[BinarySensorBinding]
 }
-case class SwitchBinding(typeString: String, writePhysDeviceId: Int, readPhysDeviceId: Int) extends SupportedDeviceBinding
+case class SwitchBinding(typeString: String, writePhysDeviceId: Int, readPhysDeviceId: Int) extends SupportedDeviceBinding {
+  override def getBoundIds: List[Int] = List(writePhysDeviceId, readPhysDeviceId)
+}
 object SwitchBinding {
   implicit val rw: ReadWriter[SwitchBinding] =
     macroRW[SwitchBinding]
 }
-case class TemperatureSensorBinding(typeString: String, physDeviceId: Int) extends SupportedDeviceBinding
+case class TemperatureSensorBinding(typeString: String, physDeviceId: Int) extends SupportedDeviceBinding {
+  override def getBoundIds: List[Int] = List(physDeviceId)
+}
 object TemperatureSensorBinding {
   implicit val rw: ReadWriter[TemperatureSensorBinding] =
     macroRW[TemperatureSensorBinding]
 }
-case class HumiditySensorBinding(typeString: String, physDeviceId: Int) extends SupportedDeviceBinding
+case class HumiditySensorBinding(typeString: String, physDeviceId: Int) extends SupportedDeviceBinding {
+  override def getBoundIds: List[Int] = List(physDeviceId)
+}
 object HumiditySensorBinding {
   implicit val rw: ReadWriter[HumiditySensorBinding] =
     macroRW[HumiditySensorBinding]
--- core/src/test/scala/ch/epfl/core/compiler/groupAddressAssigner/GroupAddressAssignerTest.scala
+++ core/src/test/scala/ch/epfl/core/compiler/groupAddressAssigner/GroupAddressAssignerTest.scala
@@ -1,7 +1,80 @@
 package ch.epfl.core.compiler.groupAddressAssigner
 
+import ch.epfl.core.models.physical.{DPT1, DPT12, DPT2, DPT5, In, InOut, Out, PhysicalDevice, PhysicalDeviceCommObject, PhysicalDeviceNode, PhysicalStructure}
+import ch.epfl.core.models.prototypical.{AppLibraryBindings, AppPrototypeBindings, BinarySensor, BinarySensorBinding, DeviceInstanceBinding, HumiditySensor, HumiditySensorBinding, Switch, SwitchBinding, TemperatureSensor, TemperatureSensorBinding}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class AssignerTest extends AnyFlatSpec with Matchers {
+class GroupAddressAssignerTest extends AnyFlatSpec with Matchers {
+  "assignGroupAddressesToPhysical" should "return an assignment in which each used id has an address and all addresses are unique" in {
+    val device1Physical = PhysicalDevice(
+      "device1",
+      ("1", "1", "1"),
+      List(
+        PhysicalDeviceNode(
+          "device1Node1",
+          List(PhysicalDeviceCommObject("device1Node1ComObj1", DPT1, In, 111))
+        )
+      )
+    )
+    val device2Physical = PhysicalDevice(
+      "device2",
+      ("1", "1", "2"),
+      List(
+        PhysicalDeviceNode(
+          "device2Node1",
+          List(
+            PhysicalDeviceCommObject("device2Node1ComObj1", DPT2, Out, 211),
+            PhysicalDeviceCommObject("device2Node1ComObj2", DPT5, In, 212)
+          )
+        )
+      )
+    )
+    val device3Physical = PhysicalDevice(
+      "device3",
+      ("1", "1", "3"),
+      List(
+        PhysicalDeviceNode(
+          "device3Node1",
+          List(
+            PhysicalDeviceCommObject("device3Node1ComObj1", DPT2, Out, 311),
+            PhysicalDeviceCommObject("device3Node1ComObj2", DPT5, In, 312)
+          )
+        ),
+        PhysicalDeviceNode(
+          "device3Node2",
+          List(
+            PhysicalDeviceCommObject("device3Node2ComObj1", DPT5, In, 321),
+            PhysicalDeviceCommObject("device3Node2ComObj2", DPT12, InOut, 322)
+          )
+        )
+      )
+    )
+    val physicalStructure = PhysicalStructure(List(device1Physical, device2Physical, device3Physical))
+
+    val appLibraryBindings = AppLibraryBindings(List(
+      AppPrototypeBindings("app1", List(
+        DeviceInstanceBinding("device1", BinarySensorBinding(BinarySensor.toString, 311)),
+        DeviceInstanceBinding("device2", SwitchBinding(Switch.toString, 211, 212)),
+        DeviceInstanceBinding("device3", TemperatureSensorBinding(TemperatureSensor.toString, 322)),
+        DeviceInstanceBinding("device4", HumiditySensorBinding(HumiditySensor.toString, 321))
+      ))
+    ))
+
+    val result = GroupAddressAssigner.assignGroupAddressesToPhysical(physicalStructure, appLibraryBindings)
+
+    result.physStruct shouldEqual physicalStructure
+    result.appLibraryBindings shouldEqual appLibraryBindings
+
+    result.physIdToGA.contains(311) shouldBe true
+    result.physIdToGA.contains(211) shouldBe true
+    result.physIdToGA.contains(212) shouldBe true
+    result.physIdToGA.contains(322) shouldBe true
+    result.physIdToGA.contains(321) shouldBe true
+
+    val ga = List(result.physIdToGA(311), result.physIdToGA(211), result.physIdToGA(212), result.physIdToGA(321), result.physIdToGA(322))
+    ga.length shouldEqual ga.toSet.size
+
+
+  }
 }
