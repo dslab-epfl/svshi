c7c3d7614623bb8199d948fbd3510fd692cc68d3
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-11-19 12:30:36 +0100

    Update runtime and verifier script to be able to kill apps

--- runtime/main.py
+++ runtime/main.py
@@ -1,9 +1,27 @@
 from verifier.verifier import Verifier
 from verifier.tracker import StompWritesTracker
 import signal
+import argparse
+import subprocess
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Runtime verifier.")
+    parser.add_argument(
+        "-l", "--list", nargs="+", help="A list of '<app_name>/<pid>'", required=True
+    )
+    args = parser.parse_args()
+    return args.list
+
 
 if __name__ == "__main__":
-    with StompWritesTracker() as tracker:
-        verifier = Verifier()
-        tracker.on_message(verifier.verify_write)
-        signal.pause()
+    apps_pids = parse_args()
+    print("Welcome to the Pistis runtime verifier!")
+    try:
+        apps = {s.split("/")[0]: s.split("/")[1] for s in apps_pids}
+        with StompWritesTracker() as tracker:
+            verifier = Verifier()
+            tracker.on_message(verifier.verify_write)
+            signal.pause()
+    except KeyboardInterrupt:
+        print("Exiting...")
--- runtime/runtime.sh
+++ runtime/runtime.sh
@@ -1,12 +1,28 @@
-# Run the Stomp server
-coilmq
+#!/bin/bash
 
-# TODO: Run the runtime verifier
+# Run the Stomp server
+coilmq &
+coilmq_pid=$!
+
+sleep 1
 
 # Run all the apps
+apps_pid=()
 cd ../app-library
 for d in */ ; do
-    cd $d
-    ./run.sh &
-    cd ..
+    python3 $d/main.py &
+    apps_pid+=($d$!)
 done
+
+cd ..
+
+sleep 1
+
+#  Run the runtime verifier
+cd runtime
+python3 main.py -l "${apps_pid[@]}"
+
+sleep 1
+
+# Kill the Stomp server
+kill $coilmq_pid
