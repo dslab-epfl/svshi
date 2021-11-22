bbb6cbfcdbe31d9a2fc444d369df157b1f523534
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-11-22 16:25:22 +0100

    Generate group_addresses.json with addresses list

--- core/src/main/scala/ch/epfl/core/utils/Constants.scala
+++ core/src/main/scala/ch/epfl/core/utils/Constants.scala
@@ -5,6 +5,8 @@ object Constants {
   val PHYSICAL_STRUCTURE_JSON_FILE_NAME = "physical_structure.json"
   val APP_PROTO_BINDINGS_JSON_FILE_NAME = "apps_bindings.json"
   val APP_PYTHON_ADDR_BINDINGS_FILE_NAME = "addresses.json"
+  val GROUP_ADDRESSES_LIST_FILE_NAME = "group_addresses.json"
   val APP_PROTO_STRUCT_FILE_NAME = "app_prototypical_structure.json"
-  val GENERATED_FOLDER_PATH_STRING = "../generated"
+  val GENERATED_FOLDER_NAME = "generated"
+  val GENERATED_FOLDER_PATH_STRING = s"../$GENERATED_FOLDER_NAME"
 }
