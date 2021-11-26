a88ad0c200c25bc885c4c885e0337b1d0af86b9d
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-11-26 17:30:54 +0100

    Use the right file for reading the group addresses

--- core_python/main.py
+++ core_python/main.py
@@ -2,7 +2,7 @@ from core_python.generator import Generator
 from core_python.parser import Parser
 
 if __name__ == "__main__":
-    parser = Parser("ga.json")
+    parser = Parser("generated/group_addresses.json")
     group_addresses_with_types = parser.parse_group_addresses()
     generator = Generator("verification_file.py", group_addresses_with_types)
     generator.generate_verification_file()
