78e52bc9e10ddc3ca356e46fabacc0484120d5fd
samuelchassot <samuel.chassot@epfl.ch>
2021-12-09 16:33:38 +0100

    run all apps on the old_state and then merge outputs

--- runtime/state.py
+++ runtime/state.py
@@ -1,6 +1,6 @@
 import json
 import dataclasses
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Tuple, Union, Set
 from xknx.core.value_reader import ValueReader
 from xknx.telegram.address import GroupAddress
 from xknx.xknx import XKNX
@@ -81,22 +81,46 @@ class State:
 
         return updated_fields
 
+    def __get_modified_fields(self, original_state: PhysicalState, other_states: List[PhysicalState]) -> Set[str]:
+        res = []
+        for other_state in other_states:
+            modified_fields = [field for field, value in self.__compare(original_state, other_state)]
+            res.append(modified_fields)
+
+        return Set(res)
+
+
+    def __merge_states(self, old_state: PhysicalState, new_states: Dict[App, PhysicalState]) -> PhysicalState:
+        # Merge all the new states with the old one.
+        # First all the non-privileged apps are run in alphabetical order, then the privileged ones in alphabetical order
+        # In this way the privileged apps can override the behavior of the non-privileged ones
+        sorted_new_states_by_priority = {k: v for k, v in sorted(new_states.items(), key=lambda item: (item[0].is_privileged, item[0].name))} 
+
+        res = dataclasses.replace(old_state)
+        for _, state in sorted_new_states_by_priority:
+            updated_fields = self.__compare(old_state, merged_state)
+            for field, value in updated_fields:  
+                setattr(res, field, value)
+        return res
+
     async def __notify_listeners(self, address: str):
         # We first execute all the listeners
         old_state = dataclasses.replace(self.__physical_state)
+        new_states = {}
         for app in self.__addresses_listeners[address]:
-            # First all the non-privileged apps are run in alphabetical order, then the privileged ones in alphabetical order
-            # In this way the privileged apps can override the behavior of the non-privileged ones
             if app.should_run:
-                old_state_before_app = dataclasses.replace(self.__physical_state)
-                app.notify(self.__physical_state)
-                if not check_conditions(self.__physical_state):
-                    # If conditions are not preserved, we revert to the previous state and prevent the app from running again
-                    self.__physical_state = old_state_before_app
+                per_app_state = dataclasses.replace(old_state)
+                app.notify(per_app_state)
+                if not check_conditions(per_app_state):
+                    # If conditions are not preserved, we prevent the app from running again
                     app.stop()
+                else:
+                    # If conditions are preserved, we keep the state to later propagate
+                    new_states[app] = per_app_state
 
         # Then we write to KNX for just the final values given to the updated fields
-        updated_fields = self.__compare(self.__physical_state, old_state)
+        merged_state = self.__merge_states(old_state, new_states)
+        updated_fields = self.__compare(old_state, merged_state)
         if updated_fields:
             for address, value in updated_fields:
                 if isinstance(value, bool):
