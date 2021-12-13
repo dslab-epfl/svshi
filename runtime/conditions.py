0937c3051377abbae7dd754fe2ddda750b636527
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-12-13 14:41:54 +0100

    Revert to default files

--- runtime/conditions.py
+++ runtime/conditions.py
@@ -1,3 +1,6 @@
-from verification_file import 
+# Default file, will be overwritten while running
+        
+from .verification_file import PhysicalState
+
 def check_conditions(state: PhysicalState) -> bool:
-    return
+    return True
