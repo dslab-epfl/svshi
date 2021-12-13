30f78990b24068b60d5a02b5723e3c5471a433d4
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-12-13 10:15:22 +0100

    Add mainargs for CLI in core

--- core/src/main/scala/ch/epfl/core/Main.scala
+++ core/src/main/scala/ch/epfl/core/Main.scala
@@ -10,40 +10,60 @@ import ch.epfl.core.verifier.exceptions.{VerifierError, VerifierInfo, VerifierMe
 
 import java.nio.file.Path
 import scala.annotation.tailrec
+import mainargs.{main, arg, ParserForClass, Flag, TokensReader}
 
-object Main extends App {
+object Main {
+
+  sealed trait Task
+  case object Compile extends Task
+  case object GenerateBindings extends Task
+
+  implicit object TaskRead
+      extends TokensReader[Task](
+        "command",
+        strs =>
+          strs.head match {
+            case "compile"          => Right(Compile)
+            case "generateBindings" => Right(GenerateBindings)
+            case token: String      => Left(token)
+          }
+      )
+
+  @main
+  case class Config(
+      @arg(name = "task", short = 't', doc = "The task to run", positional = true)
+      task: Task,
+      @arg(name = "etsProjectFile", short = 'f', doc = "The ETS project file to use", positional = true)
+      etsProjectFile: String
+  )
 
   /** args = [compile | generateBindings] ets_proj_file.knxproj
     */
-  def main(): Unit = {
-    if (args.length != 2) {
-      println("Wrong number of arguments! Exiting...")
-      return
-    }
-    val task = args(0).toLowerCase
+  def main(args: Array[String]): Unit = {
+    val config = ParserForClass[Config].constructOrExit(args)
+
     val existingAppsLibrary = loadApplicationsLibrary(APP_LIBRARY_FOLDER_PATH_STRING)
     val newAppsLibrary = loadApplicationsLibrary(GENERATED_FOLDER_PATH_STRING)
 
-    val newPhysicalStructure = EtsParser.parseEtsProjectFile(args(1))
+    val newPhysicalStructure = EtsParser.parseEtsProjectFile(config.etsProjectFile)
     val existingPhysStructPath = Path.of(existingAppsLibrary.path).resolve(Path.of(Constants.PHYSICAL_STRUCTURE_JSON_FILE_NAME))
     val existingPhysicalStructure = if (existingPhysStructPath.toFile.exists()) PhysicalStructureJsonParser.parse(existingPhysStructPath.toString) else PhysicalStructure(Nil)
-    if (task == "compile") {
-      val (compiledNewApps, compiledExistingApps, gaAssignment) = compiler.Compiler.compile(newAppsLibrary, existingAppsLibrary, newPhysicalStructure)
-      val verifierMessages = verifier.Verifier.verify(compiledNewApps, compiledExistingApps, gaAssignment)
-      if (validateProgram(verifierMessages)) {
-        // Copy new app + all files in app_library
-        FileUtils.moveAllFileToOtherDirectory(Constants.GENERATED_FOLDER_PATH_STRING, existingAppsLibrary.path)
-        printTrace(verifierMessages)
-      } else {
-        // new App is rejected
-        printTrace(verifierMessages)
-      }
-    } else if (task == "generatebindings") {
-      compiler.Compiler.generateBindingsFiles(newAppsLibrary, existingAppsLibrary, newPhysicalStructure, existingPhysicalStructure)
-    } else {
-      println(s"Unknown task $task! Exiting...")
-    }
 
+    config.task match {
+      case Compile =>
+        val (compiledNewApps, compiledExistingApps, gaAssignment) = compiler.Compiler.compile(newAppsLibrary, existingAppsLibrary, newPhysicalStructure)
+        val verifierMessages = verifier.Verifier.verify(compiledNewApps, compiledExistingApps, gaAssignment)
+        if (validateProgram(verifierMessages)) {
+          // Copy new app + all files in app_library
+          FileUtils.moveAllFileToOtherDirectory(Constants.GENERATED_FOLDER_PATH_STRING, existingAppsLibrary.path)
+          printTrace(verifierMessages)
+        } else {
+          // New App is rejected
+          printTrace(verifierMessages)
+        }
+      case GenerateBindings =>
+        compiler.Compiler.generateBindingsFiles(newAppsLibrary, existingAppsLibrary, newPhysicalStructure, existingPhysicalStructure)
+    }
   }
 
   @tailrec
@@ -58,6 +78,7 @@ object Main extends App {
       case Nil => true
     }
   }
+
   @tailrec
   def printTrace(messages: List[VerifierMessage]): Unit = {
     messages match {
@@ -73,6 +94,4 @@ object Main extends App {
       case Nil => ()
     }
   }
-
-  main()
 }
