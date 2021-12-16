e0d9a80bcbe421b0f5cc020a120ea21e10c08e2a
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-12-16 15:05:18 +0100

    Add more details on unchecked functions in skeleton main.py

--- svshi/generator/skeleton/main.py
+++ svshi/generator/skeleton/main.py
@@ -1,9 +1,12 @@
 def precond() -> bool:
     # Write the preconditions of the app here
     # It can be any boolean expressions containing the read properties of the devices and constants
+    # You CANNOT use external libraries here, nor unchecked functions
     return True
 
 
 def iteration():
     # Write your app code here
+    # You CANNOT use external libraries here, put the code inside a function whose name starts
+    # with "unchecked" and use that function instead
     print("Hello, world!")
