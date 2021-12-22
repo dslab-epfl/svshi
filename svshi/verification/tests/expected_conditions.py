ab94c54668ff6cbbe14b286f840739e4241d4257
Andrea Veneziano <andrea.veneziano@epfl.ch>
2021-12-22 09:46:18 +0100

    Fix verification test

--- svshi/verification/tests/expected_conditions.py
+++ svshi/verification/tests/expected_conditions.py
@@ -1,4 +1,4 @@
-from .verification_file import third_app_invariant, PhysicalState
+from .verification_file import first_app_invariant, second_app_invariant, third_app_invariant, PhysicalState
 
 def check_conditions(state: PhysicalState) -> bool:
-    return third_app_invariant(state)
+    return first_app_invariant(state) and second_app_invariant(state) and third_app_invariant(state)
