3521076a72cf4594a1a616b0abdfe642c82352a0
Andrea Veneziano <andrea.veneziano@epfl.ch>
2022-01-02 17:08:58 +0100

    Use bindings for unknown DPT

--- svshi/core/src/main/scala/ch/epfl/core/compiler/knxProgramming/KNXProgrammer.scala
+++ svshi/core/src/main/scala/ch/epfl/core/compiler/knxProgramming/KNXProgrammer.scala
@@ -1,14 +1,33 @@
 package ch.epfl.core.compiler.knxProgramming
 
 import ch.epfl.core.model.bindings.GroupAddressAssignment
-import ch.epfl.core.model.physical.{GroupAddress, PhysicalDevice, PhysicalDeviceCommObject, PhysicalDeviceNode, KNXDatatype}
+import ch.epfl.core.model.physical.{GroupAddress, PhysicalDevice, PhysicalDeviceCommObject, PhysicalDeviceNode, KNXDatatype, DPTUnknown}
 import ch.epfl.core.utils.Constants.ASSIGNMENTS_DIRECTORY_NAME
 import java.io.File
 import com.github.tototoshi.csv.CSVWriter
+import ch.epfl.core.model.prototypical.{AppPrototypeBindings, DeviceInstanceBinding}
 
-private case class Assignment(deviceAddress: String, nodeName: String, commObjectName: String, groupAddress: GroupAddress, datatype: KNXDatatype)
+private case class Assignment(deviceName: String, deviceAddress: String, nodeName: String, commObjectName: String, groupAddress: GroupAddress, datatype: KNXDatatype)
 
 object Programmer {
+
+  private def extractAssignments(assignment: GroupAddressAssignment): List[Assignment] = {
+    val idToGroupAddress = assignment.physIdToGA
+    val dpts = assignment.getDPTsMap
+    assignment.physStruct.deviceInstances.flatMap { case PhysicalDevice(deviceName, (a1, a2, a3), nodes) =>
+      val addressString = s"$a1.$a2.$a3"
+      nodes.flatMap { case PhysicalDeviceNode(nodeName, comObjects) =>
+        comObjects.filter(idToGroupAddress contains _.id).map { case PhysicalDeviceCommObject(objName, datatype, ioType, id) =>
+          val groupAddress = idToGroupAddress(id)
+          val dtype = datatype match {
+            case DPTUnknown => dpts(groupAddress).head
+            case d          => d
+          }
+          Assignment(deviceName, addressString, nodeName, objName, groupAddress, dtype)
+        }
+      }
+    }
+  }
 
   /** Outputs the programming file with the group address assignments for each communication object.
     *
@@ -16,15 +35,7 @@ object Programmer {
     * @param filename the output filename. Defaults to "assignment.txt"
     */
   def outputProgrammingFile(assignment: GroupAddressAssignment, filename: String = "assignment.txt"): Unit = {
-    val idToGroupAddress = assignment.physIdToGA
-    val assignments = assignment.physStruct.deviceInstances.flatMap { case PhysicalDevice(deviceName, (a1, a2, a3), nodes) =>
-      val addressString = s"$a1.$a2.$a3"
-      nodes.flatMap { case PhysicalDeviceNode(nodeName, comObjects) =>
-        comObjects.filter(idToGroupAddress contains _.id).map { case PhysicalDeviceCommObject(objName, datatype, ioType, id) =>
-          Assignment(addressString, nodeName, objName, idToGroupAddress(id), datatype)
-        }
-      }
-    }
+    val assignments = extractAssignments(assignment)
     val tree = assignments.groupBy(_.deviceAddress).map { case (deviceAddress, assignments) =>
       s"device address '$deviceAddress'\n" + assignments
         .groupBy(_.nodeName)
@@ -42,25 +53,23 @@ object Programmer {
     os.write(filePath, text)
   }
 
+  /** Outputs the CSV file with the group address assignments for each communication object.
+    *
+    * @param assignment the group address assignment data structure
+    * @param filename the output filename. Defaults to "assignment.csv"
+    */
   def outputGroupAddressesCsv(assignment: GroupAddressAssignment, filename: String = "assignment.csv"): Unit = {
     def generateLine(assignment: Assignment) =
-      List("", "", assignment.commObjectName, assignment.groupAddress.toString, "", "", "", assignment.datatype.toString, "Auto")
+      List("", "", s"${assignment.deviceName} - ${assignment.commObjectName}", assignment.groupAddress.toString, "", "", "", assignment.datatype.toString, "Auto")
 
     val header = List("Main", "Middle", "Sub", "Address", "Central", "Unfiltered", "Description", "DatapointType", "Security")
-    val idToGroupAddress = assignment.physIdToGA
-    val assignments = assignment.physStruct.deviceInstances.flatMap { case PhysicalDevice(deviceName, (a1, a2, a3), nodes) =>
-      val addressString = s"$a1.$a2.$a3"
-      nodes.flatMap { case PhysicalDeviceNode(nodeName, comObjects) =>
-        comObjects.filter(idToGroupAddress contains _.id).map { case PhysicalDeviceCommObject(objName, datatype, ioType, id) =>
-          Assignment(addressString, nodeName, objName, idToGroupAddress(id), datatype)
-        }
-      }
-    }
+
+    val assignments = extractAssignments(assignment)
 
     val directoryPath = os.Path(ASSIGNMENTS_DIRECTORY_NAME)
     if (!os.exists(directoryPath)) os.makeDir(directoryPath)
     val filePath = directoryPath / filename
-    
+
     val writer = CSVWriter.open(filePath.toIO)
     writer.writeRow(header)
     writer.writeRow(List("New main group", "", "", "0/-/-", "", "", "", "", "Auto"))
