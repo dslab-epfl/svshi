6a516244a3825ced79946d681e47fa65f52ed569
Andrea Veneziano <andrea.veneziano@epfl.ch>
2022-01-20 16:08:07 +0100

    Add ok message to plants and door_lock apps

--- prototypes/door_lock/main.py
+++ prototypes/door_lock/main.py
@@ -7,12 +7,21 @@ def invariant() -> bool:
 
 def iteration():
     if not PRESENCE_DETECTOR.is_on() and not DOOR_LOCK_SENSOR.is_on():
-        if app_state.INT_0 > 5:
-            unchecked_send_message("The door at office INN319 is still opened but nobody is there!")
-        else:
-            app_state.INT_0 += 1
+        if not app_state.BOOL_0:
+            if app_state.INT_0 > 5:
+                unchecked_send_message("The door at office INN319 is still opened but nobody is there!")
+                app_state.BOOL_0 = True
+            else:
+                app_state.INT_0 += 1
     else:
         app_state.INT_0 = 0
+        if app_state.BOOL_0:
+            if PRESENCE_DETECTOR.is_on():
+                unchecked_send_message("Someone entered the office INN319. All good!")
+            elif DOOR_LOCK_SENSOR.is_on():
+                unchecked_send_message("Someone locked the door of the office INN319. All good!")
+                
+            app_state.BOOL_0 = False
 
 
 def unchecked_send_message(msg: str) -> None:
--- prototypes/plants/main.py
+++ prototypes/plants/main.py
@@ -2,11 +2,10 @@ from instances import app_state, HUMIDITY_SENSOR
 from slack_sdk.web.client import WebClient
 
 
-def unchecked_send_message(channel: str, message: str) -> None:
-    # token = str(config("SLACK_BOT_TOKEN", cast=str))
+def unchecked_send_message(message: str) -> None:
     token = "xoxb-2702504146389-2876497796775-r21j0QnaGcyfjwEVDFrYpkYO"
     slack_client = WebClient(token=token)
-    slack_client.chat_postMessage(channel=channel, text=message)
+    slack_client.chat_postMessage(channel="#plants", text=message)
 
 
 def invariant() -> bool:
@@ -17,9 +16,14 @@ def iteration():
     humidity_level = HUMIDITY_SENSOR.read()
     if humidity_level != None and humidity_level < 20:
         if app_state.INT_0 > 20:
-            unchecked_send_message("#plants", "The plant needs water!")
+            unchecked_send_message("The plant needs water!")
+            app_state.BOOL_0 = True
             app_state.INT_0 = 0
         else: 
             app_state.INT_0 += 1
     else:
+        if app_state.BOOL_0:
+            unchecked_send_message("Someone watered the plant. All good!")
+            app_state.BOOL_0 = False
+            
         app_state.INT_0 = 0
