901cd5dba45637266c7b83aadcd173901f4548bf
Samuel Chassot <samuel.chassot@epfl.ch>
2022-01-25 14:41:19 +0100

    change prototypes for demo

--- prototypes/door_lock/main.py
+++ prototypes/door_lock/main.py
@@ -8,7 +8,7 @@ def invariant() -> bool:
 def iteration():
     if not PRESENCE_DETECTOR.is_on() and not DOOR_LOCK_SENSOR.is_on():
         if not app_state.BOOL_0:
-            if app_state.INT_0 > 5:
+            if app_state.INT_0 > 1:
                 unchecked_send_message("The door at office INN319 is still opened but nobody is there!")
                 app_state.BOOL_0 = True
             else:
